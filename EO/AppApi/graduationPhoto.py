0b31f99fd92c743eefbb51e86471a3588b20e34f
1184291824 <1184291824@qq.com>
2020-06-26 10:16:04 +0800

    毕业账单修改4

--- EO/AppApi/graduationPhoto.py
+++ EO/AppApi/graduationPhoto.py
@@ -58,10 +58,6 @@ def init_student(request):
         })
 
 
-def test(request):
-    return render(request, 'App/graduationPhoto.html')
-
-
 def login(request):
     """登录系统"""
     login_status = request.session.get('graduation_login_status', 0)
@@ -93,59 +89,59 @@ def login(request):
     })
 
 
-def index(request):
-    """返回主页"""
-    if request.session.get('graduation_login_status', 0):
-        student = GraduationStudent.objects.get(student_id=request.session.get('student_id'))
-        student_photos = GraduationStudent.objects.filter(~Q(photo='')).order_by('pk').values_list('photo', flat=True)
-
-        return render(request, 'App/graduationPhoto/index.html', {
-            'student': student,
-            'student_photos': list(student_photos),
-        })
-    else:
-        return redirect('APPApi:graduationPhoto_login')
-
-
-@csrf_exempt
-def getbody(request):
-    """人像抠图"""
-    if request.session.get('graduation_login_status', 0):
-        if request.method == "POST":
-            import requests
-
-            request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg"  # API地址
-            img = request.POST['image']  # 图片的base64编码
-
-            params = {  # 请求参数
-                "image": img,
-                "type": "foreground"
-            }
-            access_token = '24.7e1152aa822f1fda2906f38ccbf3008c.2592000.1593790511.282335-20214655'  # 访问密码
-            request_url = request_url + "?access_token=" + access_token
-            headers = {'content-type': 'application/x-www-form-urlencoded'}
-            response = requests.post(request_url, data=params, headers=headers)  # 发起访问请求
-            if response:
-                # print(response.json())
-                return JsonResponse(response.json())
-            else:
-                return HttpResponse(response)
-    else:
-        return redirect('APPApi:graduationPhoto_login')
+# def index(request):
+#     """返回主页"""
+#     if request.session.get('graduation_login_status', 0):
+#         student = GraduationStudent.objects.get(student_id=request.session.get('student_id'))
+#         student_photos = GraduationStudent.objects.filter(~Q(photo='')).order_by('pk').values_list('photo', flat=True)
+#
+#         return render(request, 'App/graduationPhoto/index.html', {
+#             'student': student,
+#             'student_photos': list(student_photos),
+#         })
+#     else:
+#         return redirect('APPApi:graduationPhoto_login')
 
 
-@csrf_exempt
-def get_photo(request):
-    """获取提交的照片"""
-    if request.session.get('graduation_login_status', 0):
-        if request.method == "POST":
-            photo = request.POST['img3']
-            student = GraduationStudent.objects.get(student_id=request.session['student_id'])
-            student.photo = photo
-            student.save()
-            return HttpResponse('success')
-    else:
-        return redirect('APPApi:graduationPhoto_login')
+# @csrf_exempt
+# def getbody(request):
+#     """人像抠图"""
+#     if request.session.get('graduation_login_status', 0):
+#         if request.method == "POST":
+#             import requests
+#
+#             request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v1/body_seg"  # API地址
+#             img = request.POST['image']  # 图片的base64编码
+#
+#             params = {  # 请求参数
+#                 "image": img,
+#                 "type": "foreground"
+#             }
+#             access_token = '24.7e1152aa822f1fda2906f38ccbf3008c.2592000.1593790511.282335-20214655'  # 访问密码
+#             request_url = request_url + "?access_token=" + access_token
+#             headers = {'content-type': 'application/x-www-form-urlencoded'}
+#             response = requests.post(request_url, data=params, headers=headers)  # 发起访问请求
+#             if response:
+#                 # print(response.json())
+#                 return JsonResponse(response.json())
+#             else:
+#                 return HttpResponse(response)
+#     else:
+#         return redirect('APPApi:graduationPhoto_login')
+#
+#
+# @csrf_exempt
+# def get_photo(request):
+#     """获取提交的照片"""
+#     if request.session.get('graduation_login_status', 0):
+#         if request.method == "POST":
+#             photo = request.POST['img3']
+#             student = GraduationStudent.objects.get(student_id=request.session['student_id'])
+#             student.photo = photo
+#             student.save()
+#             return HttpResponse('success')
+#     else:
+#         return redirect('APPApi:graduationPhoto_login')
 
 
 def student_logout(request):
@@ -154,12 +150,12 @@ def student_logout(request):
     return redirect('APPApi:graduationPhoto_login')
 
 
-def menu(request):
-    """菜单"""
-    if request.session.get('graduation_login_status', 0):
-        return render(request, "App/graduationPhoto/menu.html")
-    else:
-        return redirect("APPApi:graduationPhoto_login")
+# def menu(request):
+#     """菜单"""
+#     if request.session.get('graduation_login_status', 0):
+#         return render(request, "App/graduationPhoto/menu.html")
+#     else:
+#         return redirect("APPApi:graduationPhoto_login")
 
 
 def mybill(request):
